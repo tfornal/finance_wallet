342dd16d1488a02b4a54bbd2e3dd60b66a16c28d
tfornal <tomasz.fornal6@gmail.com>
2023-12-10 19:39:50 +0100

    working asset, delete and edit functions included

--- routers/assets.py
+++ routers/assets.py
@@ -3,9 +3,17 @@ import sys
 sys.path.append("..")
 import models
 from database import engine, SessionLocal
-from fastapi import Depends, HTTPException, APIRouter, Request, Form, status, Query
+from fastapi import (
+    Depends,
+    HTTPException,
+    APIRouter,
+    Request,
+    Form,
+    status,
+    Query,
+)
 from fastapi.responses import JSONResponse, HTMLResponse
-from sqlalchemy import func, Date
+from sqlalchemy import func, Date, event
 from sqlalchemy.orm import Session
 
 from starlette.responses import RedirectResponse
@@ -17,9 +25,6 @@ from typing import Optional
 # from pydantic import BaseModel, Field
 from .authorization import oauth2_bearer, get_current_user
 from typing import Annotated
-
-### to bedzie ptorzebne gdy bede przechodzil na html
-from fastapi.templating import Jinja2Templates
 
 
 import pandas as pd
@@ -48,6 +53,21 @@ token_dependency = Annotated[str, Depends(oauth2_bearer)]
 models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
+
+
+def calculate_percentage_share(user_id: int, db: Session):
+    assets = db.query(models.Assets).filter(models.Assets.owner_id == user_id).all()
+
+    total_asset_value = sum(asset.asset_value_pln for asset in assets)
+
+    for asset in assets:
+        asset.percentage_share = (
+            (asset.asset_value_pln / total_asset_value) * 100
+            if total_asset_value
+            else 0
+        )
+
+    db.commit()
 
 
 def format_number(number):
@@ -105,6 +125,8 @@ async def get_all_assets(request: Request, db: Session = Depends(get_db)):
     if user is None:
         return RedirectResponse(url="/authorization", status_code=302)
 
+    calculate_percentage_share(user.get("id"), db)
+
     all_assets = (
         db.query(models.Assets).filter(models.Assets.owner_id == user.get("id")).all()
     )
@@ -124,17 +146,102 @@ async def get_all_assets(request: Request, db: Session = Depends(get_db)):
     )
 
 
-@router.get("/", response_class=HTMLResponse)
-async def get_all_by_user(
+@router.get("/edit/{asset_id}", response_class=HTMLResponse)
+async def edit_asset(
+    request: Request, asset_id: int, db: SessionLocal = Depends(get_db)
+):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/authorization", status_code=status.HTTP_302_FOUND)
+    asset = db.query(models.Assets).filter(models.Assets.id == asset_id).first()
+    if not asset:
+        raise HTTPException(status_code=404, detail="Asset not found")
+    return templates.TemplateResponse(
+        "edit_asset.html", {"request": request, "asset": asset, "user": user}
+    )
+
+
+@router.post("/edit/{asset_id}", response_class=HTMLResponse)
+async def edit_expense_commit(
     request: Request,
+    asset_id: int,
+    title: str = Form(...),
+    asset_value_pln: str = Form(...),
+    category: str = Form(...),
+    db: Session = Depends(get_db),
+):
+    # breakpoint()
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/authorization", status_code=status.HTTP_302_FOUND)
+    expense = db.query(models.Assets).filter(models.Assets.id == asset_id).first()
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+    db.query(models.Assets).filter(models.Assets.id == asset_id).update(
+        {
+            "title": title,
+            "asset_value_pln": asset_value_pln,
+            "category": category,
+        }
+    )
+    db.commit()
+
+    return RedirectResponse(url="/assets", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/add_asset", response_class=HTMLResponse)
+async def add_asset(request: Request):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/authorization", status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse(
+        "add_asset.html", {"request": request, "user": user}
+    )
+
+
+@router.post("/add_asset", response_class=HTMLResponse)
+async def add_asset_commit(
+    request: Request,
+    title: str = Form(...),
+    asset_value_pln: str = Form(...),
+    category: str = Form(...),
     db: Session = Depends(get_db),
 ):
     user = await get_current_user(request)
     if user is None:
         return RedirectResponse(url="/authorization", status_code=status.HTTP_302_FOUND)
-    assets = (
-        db.query(models.Wallet).filter(models.Assets.owner_id == user.get("id")).all()
+
+    asset_model = models.Assets()
+    asset_model.title = title
+    asset_model.asset_value_pln = asset_value_pln
+    asset_model.category = category
+    asset_model.owner_id = user.get("id")
+
+    db.add(asset_model)
+    db.commit()
+
+    return RedirectResponse(url="/assets", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/delete/{asset_id}")
+async def delete_asset(
+    request: Request,
+    asset_id: int,
+    db: SessionLocal = Depends(get_db),
+):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/authorization", status_code=status.HTTP_302_FOUND)
+    asset_model = (
+        db.query(models.Assets)
+        .filter(models.Assets.id == asset_id)
+        .filter(models.Assets.owner_id == user.get("id"))
+        .first()
     )
-    return templates.TemplateResponse(
-        "assets.html", {"request": request, "assets": assets, "user": user}
-    )
+    if asset_model is None:
+        return RedirectResponse(url="/assets", status_code=status.HTTP_302_FOUND)
+
+    db.query(models.Assets).filter(models.Assets.id == asset_id).delete()
+    db.commit()
+
+    return RedirectResponse(url="/assets", status_code=status.HTTP_302_FOUND)
